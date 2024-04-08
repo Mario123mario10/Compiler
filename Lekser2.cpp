5b5086c0447194477351ccb51d7f83d30fcd67c2
mpakulsk <01169219@pw.edu.pl>
2024-04-08 19:15:53 +0200

    Wstępna wersja bez regexów

--- Lekser2.cpp
+++ Lekser2.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <iomanip> 
-#include <regex>
+#include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <map>
 #include <variant>
+#include <optional>
 #include <limits>
 #include <algorithm>
+
+const char ETX_CHAR = '\x03'; //End of Text
 
 enum class TokenType {
     IntConst,
@@ -18,118 +22,31 @@ enum class TokenType {
     Keyword,
     Symbol,
     Comment,
-    Whitespaces
+    Whitespaces,
 };
 
-using TokenValue = std::variant<int, double, std::string>;
+using TokenValue = std::variant<int, double, std::string, std::monostate>; // Dodane std::monostate dla tokenów bez wartoœci
 
 struct Token {
     TokenType type;
     TokenValue value;
-    int lineNumber;
-    int columnNumber;
-    int absoluteIndex;
+    size_t lineNumber;
+    size_t columnNumber;
+    size_t absoluteIndex;
 };
 
-class Lexer {
-public:
-    Lexer(const std::string& source) : source_(source) 
-    {
-        const std::string combinedPattern = numConstPattern + "|" + boolConstPattern + "|" + nullConstPattern + "|" +
-            stringConstPattern + "|" + idPattern + "|" + symbolPattern + "|" + commentPattern + "|" + whitespacePattern;
-        tokenRegex = std::regex(combinedPattern, std::regex_constants::ECMAScript);
-    }
-
-    std::vector<Token> tokenize()
-    {
-        std::vector<Token> tokens;
-        int currentLine = 1;
-        int currentColumn = 1;
-        size_t lastLineStart = 0;
-        size_t nextTokenStart = 0;
-        size_t processedLength = 0;
-
-
-        auto words_begin = std::sregex_iterator(source_.begin(), source_.end(), tokenRegex);
-        auto words_end = std::sregex_iterator();
-
-        for (std::sregex_iterator i = words_begin; i != words_end; ++i)
-        {
-            std::smatch match = *i;
-            std::string matchStr = match.str();
-            size_t matchStart = match.position();
-            size_t matchLength = match.length();
-
-            //Wykrywanie nieznancyh tokenow - na przyklad: "ala ma kota
-            //To nie jest string_const, bo nie konczy sie cudzyslowem. Nieznany token zaczyna sie (i w tym przypadku konczy) na pozycji znaku '"'
-            //Bo potem mamy juz ala, ktore mozemy rozpoznac jako id
-
-            if (matchStart > nextTokenStart)
-            {
-                size_t gapStart = nextTokenStart;
-                size_t gapEnd = matchStart;
-                // Znalezienie linii i kolumny dla pocz¹tku luki
-                std::string gapText = source_.substr(gapStart, gapEnd - gapStart);
-                auto gapLine = currentLine + std::count(source_.begin() + processedLength, source_.begin() + gapStart, '\n');
-                auto lineStartPos = source_.rfind('\n', gapStart - 1);
-                auto gapColumn = gapStart - (lineStartPos == std::string::npos ? 0 : lineStartPos);
-
-                throw std::runtime_error("Nieprawidlowy token na pozycji " + std::to_string(gapLine) + ":" + std::to_string(gapColumn));
-            }
-
-            processedLength += matchLength;
-            nextTokenStart = matchStart + matchLength;
-            
-            try
-            {
-                auto [type, value] = getTokenTypeAndValue(matchStr); // Rozpakowanie wyniku
-
-                // Oblicz numer linii dla aktualnego tokenu
-                if (type == TokenType::Whitespaces)
-                {
-                    size_t newLines = std::count(source_.begin() + matchStart, source_.begin() + nextTokenStart, '\n');
-                    currentLine += newLines;
-                }
-
-                // ZnajdŸ pocz¹tek ostatniej linii przed tokenem
-                size_t lineStart = source_.rfind('\n', matchStart);
-                if (lineStart == std::string::npos) lineStart = 0; // Jeœli nie znaleziono, oznacza to, ¿e jesteœmy w pierwszej linii
-                else lineStart += 1; // Przesuwamy siê za znak nowej 
-                lastLineStart = lineStart;
-
-                // Oblicz numer kolumny dla aktualnego tokenu
-                currentColumn = matchStart - lineStart + 1; // +1, bo kolumny numerujemy od 1
-
-                if (type != TokenType::Whitespaces) tokens.push_back(Token{ type, value, currentLine, currentColumn, static_cast<int>(matchStart) });
-            }
-            catch (const std::runtime_error& e) {
-                throw std::runtime_error(e.what() + std::string(" na pozycji ") + std::to_string(currentLine) + ":" + std::to_string(currentColumn));
-            }
-            
-        }
-
-        // Sprawdzenie, czy na koñcu jest nieprzetworzony fragment tekstu
-        if (nextTokenStart < source_.length())
-        {
-            size_t gapStart = nextTokenStart;
-            size_t gapEnd = source_.length();
-            // Znalezienie linii i kolumny dla pocz¹tku luki
-            std::string gapText = source_.substr(gapStart, gapEnd - gapStart);
-            auto gapLine = currentLine + std::count(source_.begin() + processedLength, source_.begin() + gapStart, '\n');
-            auto lineStartPos = source_.rfind('\n', gapStart - 1);
-            auto gapColumn = gapStart - (lineStartPos == std::string::npos ? 0 : lineStartPos);
-
-            throw std::runtime_error("Nieprawidlowy token na pozycji " + std::to_string(gapLine) + ":" + std::to_string(gapColumn));
-        }
-
-        return tokens;
-    }
-
-
-
+class Lexer
+{
 private:
     std::string source_;
-    std::regex tokenRegex;
+
+    size_t currentPosition = 0;
+    size_t currentLine = 1;
+    size_t currentColumn = 1;
+
+    const size_t maxIdentifierLength = 64;
+    const size_t maxStringLength = 1024;
+
     std::map<std::string, int> keywordMap = {
                 {"def", 0},
                 {"if", 1},
@@ -158,86 +75,246 @@ private:
         {"{", 17}, {"}", 18},
         {"[", 19}, {"]", 20},
         {"=",21},
-        {",",22},{":",23},{";",24}
+        {",",22},{":",23},{".",24}
     };
 
-    // Definicje wzorcow do pattern matchingu
-    const std::string numConstPattern = R"(0|[1-9][0-9]*(\.[0-9]+)?)";
-    const std::string boolConstPattern = R"(true|false)";
-    const std::string nullConstPattern = R"(null)";
-    const std::string stringConstPattern = R"("[^"\\]*(\\.[^"\\]*)*")";
-    const std::string idPattern = R"([A-Za-z_][A-Za-z_0-9]*)";
-    const std::string symbolPattern = R"(=|==|!=|>=|<=|<|>|\|\||&&|!|\+|\-|\*|\/|\^|\(|\)|\{|\}|\[|\]|,|\:|;)";
-    const std::string commentPattern = R"(#[^\n]*)";
-    const std::string whitespacePattern = R"(\s+)";
-
-    std::pair<TokenType, TokenValue> getTokenTypeAndValue(const std::string& token) {
-        // S³owa kluczowe
-        if (auto kw = keywordMap.find(token); kw != keywordMap.end()) return { TokenType::Keyword, kw->second };
-        // Symbole
-        if (auto sm = symbols.find(token); sm != symbols.end()) return { TokenType::Symbol, sm->second }; // Zwracamy typ Symbol i numer 
-        // Numeryczne sta³e
-        if (std::regex_match(token, std::regex(numConstPattern))) {
-            try {
-                if (token.find('.') != std::string::npos) {
-                    // Próba konwersji na double
-                    return { TokenType::DoubleConst, std::stod(token) };
-                }
-                else {
-                    // Próba konwersji na int
-                    long long tempValue = std::stoll(token); // U¿ywam std::stoll aby obs³u¿yæ wiêkszy zakres
-                    if (tempValue < std::numeric_limits<int>::min() || tempValue > std::numeric_limits<int>::max()) {
-                        throw std::out_of_range("Wartoœæ int poza zakresem");
-                    }
-                    return { TokenType::IntConst, static_cast<int>(tempValue) };
-                }
-            }
-            catch (const std::out_of_range& e) {
-                // Obs³uga sytuacji, gdy liczba jest poza zakresem
-                throw std::runtime_error("Liczba poza zakresem w tokenie: " + token);
-            }
-            catch (const std::invalid_argument& e) {
-                // Obs³uga sytuacji, gdy argument nie jest poprawn¹ liczb¹
-                throw std::runtime_error("Nieprawid³owy argument dla liczby w tokenie: " + token);
-            }
-        }
-
-        // Stringi
-        if (token[0] == '"') return { TokenType::StringConst, token.substr(1, token.size() - 2) };
-        // Komentarze
-        if (token[0] == '#') return { TokenType::Comment, {} };
-        //BoolConst
-        if (std::regex_match(token, std::regex(boolConstPattern))) return { TokenType::BoolConst, token=="true"}; //0 = false, 1 = true
-        //NullConst
-        if (token == "null") return { TokenType::NullConst, {} }; //0 = false, 1 = true
-        // Identyfikatory
-        if (std::regex_match(token, std::regex(idPattern))) return { TokenType::Id, token };
-        // W przeciwnym razie, skoro sprawdzamy wykryty token, to sa to biale znaki
-        return { TokenType::Whitespaces, token };
+    void setSource(const std::string& source)
+    {
+        source_ = source + ETX_CHAR; //Straznik na koncu, dzieki ktoremu wykryjemy nieprawidlowy token na samym koñcu
     }
 
+    char currentChar() {
+        return currentPosition < source_.length() ? source_[currentPosition] : ETX_CHAR;
+    }
+
+    void advance() {
+        if (currentChar() == '\n') {
+            currentLine++;
+            currentColumn = 0;
+        }
+        currentPosition++;
+        currentColumn++;
+    }
+
+    Token readNumber() {
+        size_t start = currentPosition;
+        while (isdigit(currentChar()) || currentChar() == '.') {
+            advance();
+        }
+
+        // Ekstrakcja tekstu reprezentuj¹cego liczbê
+        std::string numberStr = source_.substr(start, currentPosition - start);
+
+        // Sprawdzenie, czy liczba zawiera kropkê, co wskazywa³oby na liczbê zmiennoprzecinkow¹
+        if (numberStr.find('.') != std::string::npos) {
+            try {
+                double value = std::stod(numberStr);
+                return Token{ TokenType::DoubleConst, value, currentLine, currentColumn - (currentPosition - start) };
+            }
+            catch (const std::out_of_range&) {
+                throw std::runtime_error("Double value out of range at line: " + std::to_string(currentLine) + " column: " + std::to_string(currentColumn));
+            }
+        }
+        else {
+            try {
+                int value = std::stoi(numberStr);
+                return Token{ TokenType::IntConst, value, currentLine, currentColumn - (currentPosition - start) };
+            }
+            catch (const std::out_of_range&) {
+                throw std::runtime_error("Integer value out of range at line: " + std::to_string(currentLine) + " column: " + std::to_string(currentColumn));
+            }
+        }
+    }
+
+    Token readIdentifierOrKeyword() {
+        size_t start = currentPosition;
+        while (isalnum(currentChar()) || currentChar() == '_') {
+            advance();
+        }
+
+        std::string identifier = source_.substr(start, currentPosition - start);
+
+        // Sprawdzenie, czy utworzony identyfikator jest s³owem kluczowym
+        if (keywordMap.find(identifier) != keywordMap.end()) {
+            // Jeœli tak, zwróæ token reprezentuj¹cy s³owo kluczowe
+            return Token{ TokenType::Keyword, identifier, currentLine, currentColumn - (currentPosition - start) };
+        }
+        else {
+            // W przeciwnym razie zwróæ token reprezentuj¹cy identyfikator
+            return Token{ TokenType::Id, identifier, currentLine, currentColumn - (currentPosition - start) };
+        }
+    }
+
+
+    Token readString() {
+        advance(); // Pomijamy pocz¹tkowy cudzys³ów
+        std::string value;
+        size_t startLine = currentLine;
+        size_t startColumn = currentColumn;
+
+        while (currentChar() != '"' && currentChar() != ETX_CHAR) {
+            if (currentChar() == '\\') { // Obs³uga sekwencji ucieczki
+                advance(); // Przesuwamy siê, aby uzyskaæ nastêpny znak po '\'
+                switch (currentChar()) {
+                case 'n': value += '\n'; break;
+                case 't': value += '\t'; break;
+                case '\\': value += '\\'; break;
+                case '"': value += '"'; break;
+                default: value += '\\'; value += currentChar(); // Nieznana sekwencja ucieczki
+                }
+            }
+            else {
+                value += currentChar();
+            }
+            advance();
+        }
+
+        if (currentChar() == ETX_CHAR) {
+            throw std::runtime_error("String not closed before end of file at line " + std::to_string(startLine) + " column " + std::to_string(startColumn));
+        }
+
+        advance(); // Pomijamy koñcowy cudzys³ów
+
+        return Token{ TokenType::StringConst, value, startLine, startColumn };
+    }
+
+
+    Token readComment() {
+        size_t startLine = currentLine;
+        size_t startColumn = currentColumn;
+        // Pomijamy znak rozpoczynaj¹cy komentarz, tutaj '#'
+        advance();
+
+        std::string commentContent;
+        while (currentChar() != '\n' && currentChar() != ETX_CHAR) {
+            commentContent += currentChar();
+            advance();
+        }
+
+        // Nie pomijamy znaku nowej linii ('\n') na koñcu komentarza, 
+        // poniewa¿ jego obecnoœæ mo¿e byæ istotna dla innych elementów leksera,
+        // takich jak zliczanie linii.
+
+        return Token{ TokenType::Comment, commentContent, startLine, startColumn };
+    }
+
+    Token readSymbol() {
+        size_t startLine = currentLine;
+        size_t startColumn = currentColumn;
+
+        std::string potentialSymbol(1, currentChar());
+        std::string longestMatch = potentialSymbol;
+
+        // Sprawdzanie d³u¿szych symboli z³o¿onych
+        advance(); // Przesuwamy siê do przodu, aby sprawdziæ potencjalne z³o¿one symbole
+        if (!symbols.count(potentialSymbol)) {
+            throw std::runtime_error("Unknown symbol at line " + std::to_string(currentLine) + " column " + std::to_string(currentColumn));
+        }
+
+        int symbolId = symbols[potentialSymbol]; // Zak³adamy, ¿e ka¿dy symbol ma unikalne ID.
+
+        // Jeœli obecny znak mo¿e tworzyæ z³o¿ony symbol z poprzednim, sprawdŸ to
+        potentialSymbol += currentChar();
+        if (symbols.count(potentialSymbol)) {
+            longestMatch = potentialSymbol; // Znaleziono d³u¿szy, pasuj¹cy symbol
+            symbolId = symbols[longestMatch]; // Aktualizujemy ID na nowo znaleziony, d³u¿szy symbol
+            advance(); // Konsumujemy dodatkowy znak, który jest czêœci¹ z³o¿onego symbolu
+        }
+
+        return Token{ TokenType::Symbol, longestMatch, startLine, startColumn };
+    }
+
+
+    Token readWhitespace() {
+        size_t startLine = currentLine;
+        size_t startColumn = currentColumn;
+
+        while (isspace(currentChar())) {
+            // Przesuwamy siê do kolejnego znaku
+            advance();
+        }
+        return Token{ TokenType::Whitespaces, " ", startLine, startColumn};
+    }
+
+
+public:
+    
+    Lexer(size_t maxIdentifierLength = 64, size_t maxStringLength = 1024) : maxIdentifierLength(maxIdentifierLength), maxStringLength(maxStringLength) {}
+    void loadFromString(const std::string& source) { setSource(source); } //Obsluga zrodla: ciag znakow
+    void loadFromFile(const std::string& filePath) //Obsluga zrodla: plik
+    {
+        std::ifstream file(filePath);
+        if (!file) throw std::runtime_error("Nieudany odczyt z pliku: " + filePath);
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        setSource(buffer.str());
+    }
+
+    std::optional<Token> nextToken()
+    {
+        while (currentChar() != ETX_CHAR) {
+            if (isspace(currentChar())) {
+                return readWhitespace();
+            }
+            else if (isdigit(currentChar())) {
+                return readNumber();
+            }
+            else if (isalpha(currentChar()) || currentChar() == '_') {
+                return readIdentifierOrKeyword();
+            }
+            else if (currentChar() == '"') {
+                return readString();
+            }
+            else if (currentChar() == '#') {
+                return readComment();
+            }
+            else {
+                return readSymbol();
+            }
+        }
+        return std::nullopt; // Zakoñczenie pliku
+    }
 };
 
 int main() {
     std::string sourceCode = R"(
         def factorial(n) {
             if (n == 0) {
-                return 1;
+                return 1
             } else {
-                return n * factorial(n - 1);
+                return n * factorial(n - 1)
             }
         }
-        print("Factorial of 5 is ", factorial(5));
-        "ala ma kota"
-        x = 88888888888888888888888
+
+        print("Factorial of 5 is ", factorial(5))
+        "ala ma \"kota\"\naaaaaa" "basia ma helikopter"
+        #x = 888888888333333333333333333333333333333333333333333333333333333333
+x=3y=7
+x=3y=7.222 .2
+        
     )";
 
-    Lexer lexer(sourceCode);
+    Lexer lexer(10,20);
+    lexer.loadFromString(sourceCode);
+
+    try
+    {
+        //lexer.loadFromFile("kod.txt");
+    }
+    catch (const std::runtime_error& error)
+    {
+        std::cerr << error.what() << std::endl;
+        return 1;
+    }
+        
     std::vector<Token> tokens;
     try
     {
-        tokens = lexer.tokenize();
-        std::cout << "Tokenizacja zakonczona sukcesem.\n";
+        while (auto tokenOpt = lexer.nextToken())
+        {
+            auto token = tokenOpt.value();
+            tokens.push_back(token);
+        }
     }
     catch (const std::runtime_error& error)
     {
@@ -245,7 +322,7 @@ int main() {
         return 1;
     }
     
-
+ 
     std::cout << "Tokens:\n";
     std::cout << std::left << std::setw(20) << "Type" << std::setw(10) << "Value" << "Position (Line:Column)\n";
     std::cout << std::string(60, '-') << '\n';
@@ -262,7 +339,7 @@ int main() {
         case TokenType::Keyword: tokenType = "Keyword"; break;
         case TokenType::Symbol: tokenType = "Symbol"; break;
         case TokenType::Comment: tokenType = "Comment"; break;
-        default: tokenType = "Unknown";
+        default: tokenType = "Whitespaces";
         }
 
         std::string value = std::visit([](auto&& arg) -> std::string {
@@ -278,7 +355,7 @@ int main() {
             }
             }, token.value);
 
-        std::cout << std::setw(20) << tokenType << std::setw(10) << value << token.lineNumber << ":" << token.columnNumber << '\n';
+        std::cout << std::setw(20) << tokenType << std::setw(20) << value << token.lineNumber << ":" << token.columnNumber << '\n';
     }
 
     return 0;
